4ca4c2a5f90ccf0900f3332ec5f4de13a310a45f
Luciano Marchueta <lucianomarchueta@gmail.com>
2022-11-24 20:36:56 -0300

    cuerpo

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const productsRouter = require('./server/products');
+const cartRouter = require('./server/cart');
